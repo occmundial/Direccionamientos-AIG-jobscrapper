db8cfb66329a31c65b62f9ec20478a6803909ee8
Arturo Bravo Baez <abravob2712@gmail.com>
2025-04-04 11:32:20 -0600

    Se setea a 20 el Timeout de la cadena de conexión.

--- pkg/db_conn/db_connection.py
+++ pkg/db_conn/db_connection.py
@@ -2,16 +2,20 @@ from internal.config import configuration as c
 from pkg.slack_pkg import slack_func as sf
 import logging
 import pyodbc
+import socket
+
 
 
 def init_db_connection():
     try:
+        print("Resolviendo host:", c.host)
+        print("IP:", socket.gethostbyname(c.host))
         conn_str = ("Driver={ODBC Driver 17 for SQL Server};"
                                 f"Server={c.host};"
                                 f"UID={c.user};"
                                 f"PWD={c.password};"
                                 f"Database={c.db_name};"
-                                "Connection Timeout=50;")
+                                "Connection Timeout=20;")
         print(conn_str)
         db_conn = pyodbc.connect(conn_str,
                                  readonly=True)
